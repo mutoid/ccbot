b38e6731e1f3aa9047cde2c58b2831b87b8364e7
Mutoid <mutoidman@hotmail.com>
2015-09-30 16:47:50 -0700

    Logging and test code

--- bot_logic.rb
+++ bot_logic.rb
@@ -3,29 +3,25 @@ require 'sinatra/base'
 require 'net/http'
 require 'uri'
 require 'json'
-require 'logger'
 
 SLACK_DOMAIN = ENV['SLACK_DOMAIN']
 SLACKBOT_ENDPOINT = ENV['SLACKBOT_ENDPOINT']
 SLACKBOT_TOKEN = ENV['SLACKBOT_TOKEN']
 
-logger = Logger.new(STDOUT)
-
 class BotLogic < Sinatra::Base
   get('/') do
-    logger.info "Processing get / request"
-    puts "Got get"
+    puts "Processing get / request"
     "I'm up."
   end
 
   post('/lenny') do
-    logger.info "Processing /lenny command"
-    puts "Got post"
+    puts "Processing /lenny command"
     # post response to $SLACK_DOMAIN$SLACKBOT_ENDPOINT$SLACKBOT_TOKEN
     response_text = "( ͡° ͜ʖ ͡°)"
     begin
       uri = URI.parse("#{SLACK_DOMAIN}#{SLACKBOT_ENDPOINT}")
       response = Net::HTTP.post_form(uri, {"token" => SLACKBOT_TOKEN, "data" => response_text })
+      puts response
     rescue Exception => e
       logger.info "Got exception #{e}"
       puts "WTF!"
