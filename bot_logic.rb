0981c150f1e5b01f44d8b2dbe0433ff3aa0de528
Mutoid <mutoidman@hotmail.com>
2015-09-30 16:14:55 -0700

    Add some logging

--- bot_logic.rb
+++ bot_logic.rb
@@ -8,16 +8,25 @@ SLACK_DOMAIN = ENV['SLACK_DOMAIN']
 SLACKBOT_ENDPOINT = ENV['SLACKBOT_ENDPOINT']
 SLACKBOT_TOKEN = ENV['SLACKBOT_TOKEN']
 
+logger = Logger.new('app.log', 10, 1024000)
+
 class BotLogic < Sinatra::Base
   get('/') do
+    logger.info "Processing get / request"
     "I'm up."
   end
 
   post('/lenny') do
+    logger.info "Processing /lenny command"
     # post response to $SLACK_DOMAIN$SLACKBOT_ENDPOINT$SLACKBOT_TOKEN
     response_text = "( ͡° ͜ʖ ͡°)"
-    uri = URI.parse("#{SLACK_DOMAIN}#{SLACKBOT_ENDPOINT}")
-    response = Net::HTTP.post_form(uri, {"token" => SLACKBOT_TOKEN, "data" => response_text })
+    begin
+      uri = URI.parse("#{SLACK_DOMAIN}#{SLACKBOT_ENDPOINT}")
+      response = Net::HTTP.post_form(uri, {"token" => SLACKBOT_TOKEN, "data" => response_text })
+    rescue Exception => e
+      logger.info "Got exception #{e}"
+      raise e
+    end
   end
 
   run! if app_file == $0
