e93ca77856df1d00cd2de60547295be28d953159
Mutoid <mutoidman@hotmail.com>
2015-09-30 18:03:23 -0700

    use_ssl=true thanks http://stackoverflow.com/a/9227933

--- bot_logic.rb
+++ bot_logic.rb
@@ -16,22 +16,19 @@ class BotLogic < Sinatra::Base
 
   post('/lenny') do
     puts "Processing /lenny command"
-    # post response to $SLACK_DOMAIN$SLACKBOT_ENDPOINT$SLACKBOT_TOKEN
-    lenny = "( ͡° ͜ʖ ͡°)"
-    puts params
+    puts "Params: ", params
     channel = params[:channel_id]
+
+    lenny = "( ͡° ͜ʖ ͡°)"
+
     begin
       uri = URI.parse("#{SLACK_DOMAIN}#{SLACKBOT_ENDPOINT}?token=#{SLACKBOT_TOKEN}&channel=#{channel}")
-#      params = {
-#        token: SLACKBOT_TOKEN,
-#        channel: channel,
-#        body: lenny
-#      }
-#      puts "Posting #{params.inspect} to url #{uri}"
       http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
       request = Net::HTTP::Post.new(uri)
       request.body = lenny
       response = http.request(request)
+
       puts response
       puts response.body
     rescue Exception => e
