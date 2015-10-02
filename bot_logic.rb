6dff9267035bb394af89a24c5406e4e1491a2eee
Mutoid <mutoidman@hotmail.com>
2015-10-02 11:11:34 -0700

    Add hard-coded power users list. Add lenny frequency game.

--- bot_logic.rb
+++ bot_logic.rb
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 # -*- coding: utf-8 -*-
 require 'sinatra/base'
 require 'net/http'
@@ -15,6 +17,8 @@ SLACKBOT_ENDPOINT = ENV['SLACKBOT_ENDPOINT']
 SLACKBOT_TOKEN = ENV['SLACKBOT_TOKEN']
 FAKE_RESPONSE = ENV['SINATRA_ENV'] != 'production'
 
+POWER_USERS = ["mutoid", "gaywallet"]
+
 class BotLogic < Sinatra::Base
   get('/') do
     puts "Processing get / request"
@@ -29,25 +33,27 @@ class BotLogic < Sinatra::Base
     channel = params[:channel_id]
     user_name = params[:user_name]
     user_id = params[:user_id]
+    power_user = POWER_USERS.include? user_name
     command_parts = params[:command].split(' ')
     command = command_parts.first
 
-    lenny = "( ͡° ͜ʖ ͡°)"
-    
-    # break "Wait a bit, will ya?" if @last_lenny && @last_lenny + 20 > Time.now
-
-    # @last_lenny = Time.now
+    lennys = ["( ͡° ͜ʖ ͡°)",
+              "( ͡o ͜ʖ ͡o)",
+              "ᕦ( ͡° ͜ʖ ͡°)ᕤ You did it!"]
 
     commands_by_user = RunCommand.where user_id: user_id, command: command
     puts "#{user_name} has run this command #{commands_by_user.size} times."
 
     if commands_by_user.size > 0
       last_lenny = commands_by_user.last
-      break "Wait a bit, will ya?" if last_lenny.created_at + 30.seconds > Time.now && user_name != "gaywallet"
+      break "Wait a bit, will ya?" if last_lenny.created_at + 1.minute > Time.now && !power_user
     end
 
     new_command = RunCommand.new user_id: user_id, user_name: user_name, command: command
     new_command.save
+
+    lenny_index = [RunCommand.since(Time.now - 30.seconds) - 1, 2].max
+    lenny = lennys[lenny_index]
 
     if FAKE_RESPONSE
       puts "#{user_name} did a lenny."
