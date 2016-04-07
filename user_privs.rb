9daa4c997cd898bd44f2b09bdedfc822a2c49dd2
Mutoid <mutoidman@hotmail.com>
2016-04-07 16:15:38 -0700

    Privileges fix

--- user_privs.rb
+++ user_privs.rb
@@ -5,6 +5,6 @@ class UserPrivilege < ActiveRecord::Base
   def self.user_privs user_id
     user = UserPrivilege.where(user_id: user_id).first
     return [false, false] if !user
-    return [user.power_user, user.admin_user]
+    return [user.power_user == 1, user.admin_user == 1]
   end
 end
