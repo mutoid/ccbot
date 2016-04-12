6577b176d3acecbd62b668dea5534d7a9a7d7b24
Mutoid <mutoidman@hotmail.com>
2016-04-12 15:32:12 -0700

    Automatic NSFW support by channel name

--- gifme_logic.rb
+++ gifme_logic.rb
@@ -16,7 +16,7 @@ class GifmeLogic
     url
   end
 
-  def self.process(params, sfw=true)
+  def self.process(params)
     channel = params[:channel_id]
     user_name = params[:user_name]
     user_id = params[:user_id]
@@ -24,12 +24,13 @@ class GifmeLogic
     command = params[:command]
     terms = params[:text]
     query_string = terms.gsub(' ','+')
+    sfw = channel_name.downcase.include? "nsfw"
 
     new_command = RunCommand.new user_id: user_id, user_name: user_name, command: command
     new_command.save
 
     ### DO EXTERNAL REQUEST ###
-    uri = URI.parse("http://api.gifme.io/v1/search?key=#{GIFME_API_KEY}&sfw=#{sfw}&limit=10&query=#{query_string}")
+    uri = URI.parse("http://api.gifme.io/v1/search?key=#{GIFME_API_KEY}&sfw=#{sfw ? 'true' : 'false' }&limit=10&query=#{query_string}")
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = false
     request = Net::HTTP::Get.new(uri)
@@ -48,6 +49,6 @@ class GifmeLogic
     print " => #{final_url}" if final_url != image_url
     print "\n"
 
-    Chat.new(channel).chat_out "_#{user_name} searched gifme.io for '#{terms}':_\n#{final_url}"
+    Chat.new(channel).chat_out "_#{user_name} searched gifme.io for '#{terms}' #{'(nsfw ok)' if !sfw:_\n#{final_url}"
   end
 end
