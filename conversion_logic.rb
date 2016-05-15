53ebab07198ce335d141e3f386beb626f529ac77
Joel Christiansen <greenbadge.jc@gmail.com>
2016-05-14 20:28:01 -0700

    fix temp converstions

--- conversion_logic.rb
+++ conversion_logic.rb
@@ -189,10 +189,10 @@ TABLE = {
            Meter => 0.0254},
   Pound => {Kilogram => 0.453592},
   Kilogram => {Pound => 2.20462},
-  Fahrenheit => { {Celsius => Proc.new { |f| (f - 32) * 5.0 / 9.0 } },
-                 {Kelvin => Proc.new { |f| (f + 459.67) * 5.0 / 9.0 } }},
-  Celsius => { {Fahrenheit => Proc.new { |c| (c * 9.0 / 5.0) + 32 } },
-               {Kelvin => Proc.new { |c| (c + 273.15) } } },
-  Kelvin => { {Celsius => Proc.new { |k| (k - 273.15) } },
-              {Fahrenheit => Proc.new { |k| (k * 9.0 / 5.0) - 459.67 } }}
+  Fahrenheit => { Celsius => Proc.new { |f| (f - 32) * 5.0 / 9.0 },
+                 Kelvin => Proc.new { |f| (f + 459.67) * 5.0 / 9.0 }},
+  Celsius => { Fahrenheit => Proc.new { |c| (c * 9.0 / 5.0) + 32 },
+               Kelvin => Proc.new { |c| (c + 273.15) } },
+  Kelvin => { Celsius => Proc.new { |k| (k - 273.15) },
+              Fahrenheit => Proc.new { |k| (k * 9.0 / 5.0) - 459.67 } }
 }
