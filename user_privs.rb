0d909712cfd187b1cac0d082f4c0486b034a9456
mutoid <mutoidman@hotmail.com>
2016-07-01 14:43:13 -0700

    Add quick user privilege edit mode

--- user_privs.rb
+++ user_privs.rb
@@ -2,9 +2,16 @@ require 'sinatra'
 require 'sinatra/activerecord'
 
 class UserPrivilege < ActiveRecord::Base
-  def self.user_privs user_id
+  def self.user_privs user_id, new_values = {}
     user = UserPrivilege.where(user_id: user_id).first
-    return [false, false] if !user
+    return [false, false] if !user && new_values.empty?
+    
+    if !new_values.empty?
+      user.power_user = new_values[:power_user] if new_values[:power_user]
+      user.admin_user = new_values[:admin_user] if new_values[:admin_user]
+      user.save!
+    end
+    
     return [user.power_user == 1, user.admin_user == 1]
   end
 end
