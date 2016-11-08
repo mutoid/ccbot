00ce15b7cfddd867f738eeb8e43eb6a07a051c90
mutoid <mutoidman@hotmail.com>
2016-11-08 01:27:31 -0600

    Migrated server

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160317225850) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "megamojis", force: :cascade do |t|
     t.string   "base_name"
     t.integer  "width"
