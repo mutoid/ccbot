a4c78d0433525ca0edb036173fa11f360ea7dcfe
Mutoid <mutoidman@hotmail.com>
2016-11-21 13:19:37 -0800

    I have no idea what I'm doing

--- bot_logic.rb
+++ bot_logic.rb
@@ -204,3 +204,19 @@ end
 def emoji_word word
   word.gsub /(\w)/, ':\1\1:'
 end
+
+def update_users_table
+  commands = RunCommand.arel_table
+  user_ids = commands.project(commands[:id].maximum.as('max')).group(:user_id)
+  most_recent_commands = RunCommand.find_by_sql("select * from run_commands where id in (#{user_ids.to_sql})").map(&:user_name)
+  most_recent_commands.each do |command|
+    u = User.with_user_id(command.user_id)
+    if !u.nil?
+      u = User.new
+    end
+    u.name = command.user_name
+    u.updated_at = command.created_at
+    u.user_id = command.user_id
+    u.save!
+  end
+end
--- user.rb
+++ user.rb
@@ -1,10 +1,10 @@
-class User
-  attr_accessor :user_name, :user_id
-
-  def initialize(user_name, user_id)
-    @user_name, @user_id = user_name, user_id
-  end
-
+class User < ActiveRecord.base
+  # user_id: string
+  # user_name: string
+  scope named(name) -> { where("user_name = ?", user_name) }
+  scope with_user_id(user_id) -> { where("user_id = ?", user_id) }
+  scope active_within(duration) -> { where("updated_at > ?", duration.ago) }
+  
   def ==(other)
     user_id == other.user_id
   end
