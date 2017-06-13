09ba0e5534988947805cbe98160a815e4a2d7c49
Joel <greenbadge.jc@gmail.com>
2017-06-13 16:59:53 -0700

    can now handle multiple rolls

--- bot_logic.rb
+++ bot_logic.rb
@@ -69,15 +69,25 @@ class BotLogic < Sinatra::Base
   post('/roll') do
     begin
       puts "Rolling dice"
-      n, m, modifier = params[:text].scan(/-?\d+/)
-      accum = [] 
-      n = n.to_i
-      m = m.to_i
-      if (n <= 0 or m <= 0) or (n > 20 and m > 20) or (n > 100 or m > 100) # Non-numeric chars = 0, this limits size
-        break "Invalid roll"
+      rolls = params[:text].scan(/\d+d\d+(?:(?:\+|-)\d+)?(?!d)/) #splits up the rolls, also restricts the format a roll can be in (must be ndm+modifier, modifier optional)
+      if rolls.length > 5
+          break "Too many rolls"
       end
-      n.to_i.times {
-        accum.append(Random.new.rand(m.to_i) + 1)
+      accum = []
+      accum_display = []
+      rolls.each { |roll|
+        n, m, modifier = roll.scan(/-?\d+/)
+        n = n.to_i
+        m = m.to_i
+        if (n <= 0 or m <= 0) or (n > 20 and m > 20) or (n > 100 or m > 100) # Non-numeric chars = 0, this limits size
+          break "Invalid roll"
+        end
+        modifier_sum += modifier.to_i
+        n.to_i.times {
+          rand_num = Random.new.rand(m.to_i) + 1
+          accum.append(rand_num)
+          accum_display.append("#{rand_num}/#{m}")
+        }
       }
 
 
