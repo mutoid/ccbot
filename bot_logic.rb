bcd3018f2baa0a6558a967e1b814cac3d1c568a8
Joel <greenbadge.jc@gmail.com>
2017-06-13 15:36:47 -0700

    forgot modifier last time

--- bot_logic.rb
+++ bot_logic.rb
@@ -82,7 +82,7 @@ class BotLogic < Sinatra::Base
 
 
       channel = params[:channel_id]
-      output = "#{params[:user_name]} rolled (#{params[:text]}) - " + (accum.to_s) + " -  #{accum.sum}"
+      output = "#{params[:user_name]} rolled (#{params[:text]}) - " + (accum.to_s) + " -  #{accum.sum + modifier}"
       puts "roll done"
       Chat.new(channel).chat_out(output)
     rescue
